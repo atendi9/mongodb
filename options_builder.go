f00ca725c37190b497a3c24994ef00762019550c
gabrielluizsf <93672097+gabrielluizsf@users.noreply.github.com>
2026-06-25 09:06:47 -0300

    feat: adding options  builders
    BuildInsertManyOptions, BuildCountOptions, BuildDistinctOptions, BuildFindOneAndUpdateOptions, BuildFindOneAndDeleteOptions, and BuildFindOneAndReplaceOptions

--- options_builder.go
+++ options_builder.go
@@ -120,6 +120,112 @@ func BuildUpdateManyOptions(
 	return updateManyOpts
 }
 
+func BuildInsertManyOptions(
+	opts ...*options.InsertManyOptions,
+) options.Lister[options.InsertManyOptions] {
+	insertManyOpts := options.InsertMany()
+	if len(opts) > 0 {
+		opts := opts[0]
+		insertManyOpts = setOption(insertManyOpts, opts.BypassDocumentValidation, insertManyOpts.SetBypassDocumentValidation)
+		insertManyOpts = setOption(insertManyOpts, opts.Ordered, insertManyOpts.SetOrdered)
+		insertManyOpts = setOption(insertManyOpts, &opts.Comment, insertManyOpts.SetComment)
+	}
+	return insertManyOpts
+}
+
+func BuildCountOptions(
+	opts ...*options.CountOptions,
+) options.Lister[options.CountOptions] {
+	countOpts := options.Count()
+	if len(opts) > 0 {
+		opts := opts[0]
+		countOpts = setOption(countOpts, opts.Limit, countOpts.SetLimit)
+		countOpts = setOption(countOpts, opts.Skip, countOpts.SetSkip)
+		countOpts = setOption(countOpts, &opts.Comment, countOpts.SetComment)
+		countOpts = setOption(countOpts, &opts.Hint, countOpts.SetHint)
+		if opts.Collation != nil {
+			countOpts = countOpts.SetCollation(opts.Collation)
+		}
+	}
+	return countOpts
+}
+
+func BuildDistinctOptions(
+	opts ...*options.DistinctOptions,
+) options.Lister[options.DistinctOptions] {
+	distinctOpts := options.Distinct()
+	if len(opts) > 0 {
+		opts := opts[0]
+		distinctOpts = setOption(distinctOpts, &opts.Comment, distinctOpts.SetComment)
+		distinctOpts = setOption(distinctOpts, &opts.Hint, distinctOpts.SetHint)
+		if opts.Collation != nil {
+			distinctOpts = distinctOpts.SetCollation(opts.Collation)
+		}
+	}
+	return distinctOpts
+}
+
+func BuildFindOneAndUpdateOptions(
+	opts ...*options.FindOneAndUpdateOptions,
+) options.Lister[options.FindOneAndUpdateOptions] {
+	findOneAndUpdateOpts := options.FindOneAndUpdate()
+	if len(opts) > 0 {
+		opts := opts[0]
+		findOneAndUpdateOpts = setOption(findOneAndUpdateOpts, &opts.ArrayFilters, findOneAndUpdateOpts.SetArrayFilters)
+		findOneAndUpdateOpts = setOption(findOneAndUpdateOpts, opts.BypassDocumentValidation, findOneAndUpdateOpts.SetBypassDocumentValidation)
+		findOneAndUpdateOpts = setOption(findOneAndUpdateOpts, opts.ReturnDocument, findOneAndUpdateOpts.SetReturnDocument)
+		findOneAndUpdateOpts = setOption(findOneAndUpdateOpts, opts.Upsert, findOneAndUpdateOpts.SetUpsert)
+		findOneAndUpdateOpts = setOption(findOneAndUpdateOpts, &opts.Sort, findOneAndUpdateOpts.SetSort)
+		findOneAndUpdateOpts = setOption(findOneAndUpdateOpts, &opts.Projection, findOneAndUpdateOpts.SetProjection)
+		findOneAndUpdateOpts = setOption(findOneAndUpdateOpts, &opts.Comment, findOneAndUpdateOpts.SetComment)
+		findOneAndUpdateOpts = setOption(findOneAndUpdateOpts, &opts.Hint, findOneAndUpdateOpts.SetHint)
+		findOneAndUpdateOpts = setOption(findOneAndUpdateOpts, &opts.Let, findOneAndUpdateOpts.SetLet)
+		if opts.Collation != nil {
+			findOneAndUpdateOpts = findOneAndUpdateOpts.SetCollation(opts.Collation)
+		}
+	}
+	return findOneAndUpdateOpts
+}
+
+func BuildFindOneAndDeleteOptions(
+	opts ...*options.FindOneAndDeleteOptions,
+) options.Lister[options.FindOneAndDeleteOptions] {
+	findOneAndDeleteOpts := options.FindOneAndDelete()
+	if len(opts) > 0 {
+		opts := opts[0]
+		findOneAndDeleteOpts = setOption(findOneAndDeleteOpts, &opts.Sort, findOneAndDeleteOpts.SetSort)
+		findOneAndDeleteOpts = setOption(findOneAndDeleteOpts, &opts.Projection, findOneAndDeleteOpts.SetProjection)
+		findOneAndDeleteOpts = setOption(findOneAndDeleteOpts, &opts.Comment, findOneAndDeleteOpts.SetComment)
+		findOneAndDeleteOpts = setOption(findOneAndDeleteOpts, &opts.Hint, findOneAndDeleteOpts.SetHint)
+		findOneAndDeleteOpts = setOption(findOneAndDeleteOpts, &opts.Let, findOneAndDeleteOpts.SetLet)
+		if opts.Collation != nil {
+			findOneAndDeleteOpts = findOneAndDeleteOpts.SetCollation(opts.Collation)
+		}
+	}
+	return findOneAndDeleteOpts
+}
+
+func BuildFindOneAndReplaceOptions(
+	opts ...*options.FindOneAndReplaceOptions,
+) options.Lister[options.FindOneAndReplaceOptions] {
+	findOneAndReplaceOpts := options.FindOneAndReplace()
+	if len(opts) > 0 {
+		opts := opts[0]
+		findOneAndReplaceOpts = setOption(findOneAndReplaceOpts, opts.BypassDocumentValidation, findOneAndReplaceOpts.SetBypassDocumentValidation)
+		findOneAndReplaceOpts = setOption(findOneAndReplaceOpts, opts.ReturnDocument, findOneAndReplaceOpts.SetReturnDocument)
+		findOneAndReplaceOpts = setOption(findOneAndReplaceOpts, opts.Upsert, findOneAndReplaceOpts.SetUpsert)
+		findOneAndReplaceOpts = setOption(findOneAndReplaceOpts, &opts.Sort, findOneAndReplaceOpts.SetSort)
+		findOneAndReplaceOpts = setOption(findOneAndReplaceOpts, &opts.Projection, findOneAndReplaceOpts.SetProjection)
+		findOneAndReplaceOpts = setOption(findOneAndReplaceOpts, &opts.Comment, findOneAndReplaceOpts.SetComment)
+		findOneAndReplaceOpts = setOption(findOneAndReplaceOpts, &opts.Hint, findOneAndReplaceOpts.SetHint)
+		findOneAndReplaceOpts = setOption(findOneAndReplaceOpts, &opts.Let, findOneAndReplaceOpts.SetLet)
+		if opts.Collation != nil {
+			findOneAndReplaceOpts = findOneAndReplaceOpts.SetCollation(opts.Collation)
+		}
+	}
+	return findOneAndReplaceOpts
+}
+
 func setOption[O any, V any](
 	builder *O,
 	value *V,
